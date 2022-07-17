3f7d387b9ebc75b9f40dd9ec086e3e70947ee126
ihatemilk <vyckluchi75@gmail.com>
2022-07-18 01:06:44 +0300

    fix imports

--- elasticmapper/orm_mappings/__init__.py
+++ elasticmapper/orm_mappings/__init__.py
@@ -1,6 +1,6 @@
-from orm_mappings.sqlalchemy_mapping import mapping as sqlalchemy_mapping
-from orm_mappings.peewee_mapping import mapping as peewee_mapping
-from orm_mappings.django_mapping import mapping as django_mapping
+from elasticmapper.orm_mappings.sqlalchemy_mapping import mapping as sqlalchemy_mapping
+from elasticmapper.orm_mappings.peewee_mapping import mapping as peewee_mapping
+from elasticmapper.orm_mappings.django_mapping import mapping as django_mapping
 
 __all__ = (
     sqlalchemy_mapping,
